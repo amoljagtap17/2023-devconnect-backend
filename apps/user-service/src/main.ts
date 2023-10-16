10725e2e603d099960c0589f9a9c64e8b14c3cec
amoljagtap17 <amoljagtap17@gmail.com>
2023-10-16 20:50:15 +0530

    devconnect: add config, validation and helmet to user-service apps

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { UserServiceModule } from './user-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserServiceModule);
+
+  app.enableCors();
+
+  app.use(helmet());
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(3000);
 }
